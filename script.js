9afd9d68eb29437c2128a99a265736280d1ffc72
AHLAWY22 <ahmedeid.eid2006@gmail.com>
2025-04-28 21:05:22 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,1 @@
-function login() {
-  var username = document.getElementById("username").value.trim();
-  var password = document.getElementById("password").value.trim();
 
-  if (username === "ENG" && password === "1234") {
-    window.location.href = "eng.html";
-    return false;
-  } else {
-    alert("Incorrect Username or Password!");
-    return false;
-  }
-}
